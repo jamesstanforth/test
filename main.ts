a96ee530ee77109e718eb5a3dec27e471c07448e
jamesstanforth <6414210+jamesstanforth@users.noreply.github.com>
2021-02-01 12:36:03 +0000

    purple background

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     statusbar.value += -10
 })
 let statusbar: StatusBarSprite = null
-scene.setBackgroundColor(9)
+scene.setBackgroundColor(10)
 statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.setLabel("Health")
-statusbar.positionDirection(CollisionDirection.Bottom)
-statusbar.setOffsetPadding(0, 10)
+statusbar.positionDirection(CollisionDirection.Top)
+statusbar.setOffsetPadding(-50, 10)
 statusbar.value = 100
